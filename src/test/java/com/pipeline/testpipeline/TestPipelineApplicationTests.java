ce04f6ec431e3184f6ae032c58448fa929038ba3
Vinícius Ramaciotti <ramaciotti86@gmail.com>
2021-02-06 17:48:47 +0000

    First Commit
    Basic Spring project just to test the pipeline

---
+++ src/test/java/com/pipeline/testpipeline/TestPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.testpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
